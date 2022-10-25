949adc19564f4680c3b32c61b160ead0d1488804
Gerzain Magaña <gerzainnb@gmail.com>
2022-10-24 20:10:22 -0500

    Login Inputs Validated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 //React
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 //Styles
 import '../styles/Login.scss';
@@ -9,43 +9,103 @@ import LoginIllustration from '../assets/Illustrations/Scenes05.svg';
 
 
 
-export default function Login(){
+export default function Login(){ 
 
     const [loginData, setState] = useState({
         email:'',
         password:'',
-    })
+        errorMessages:{
+            invalidEmail:'',
+            invalidPassword:'',
+            loginErrorMessage:''
+        },        
+    });
+    
 
-    const Login = (e)=>{
-        e.preventDefault();        
+    const Login = (e)=>{        
+
     }
 
-    const catchInputs = (e)=>{
-        let {value, name} = e.target;                 
+
+    const catchInputs = (e)=>{                        
+        let {value, name} = e.target;        
+
         setState({
             ...loginData,
             [name]:value
-        }         
-        )      
+        })
+
+        validateInputs(name, value);       
     }
 
-    return(
+    const validateInputs = (name, value)=>{        
+        const passRegex = /^[a-zA-Z0-9\.\_]{6,20}$/;
+        const emailRegex = /^[áéíóúÁÉÍÓÚa-zA-Z0-9\.\_]+@[áéíóúÁÉÍÓÚa-zA-Z]+\.[A-Za-z]{1,8}$/;        
+
+        if(name === 'email'){
+            if(!emailRegex.test(value)){                
+                setState({
+                    ...loginData,
+                    errorMessages:{
+                        ...loginData.errorMessages,
+                        invalidEmail:'Type a valid email*'
+                    }
+                })                  
+            }else{
+                setState({
+                    ...loginData,
+                    email:value,
+                    errorMessages:{
+                        ...loginData.errorMessages,
+                        invalidEmail:''
+                    }
+                })  
+            }
+        }else{
+            if(!passRegex.test(value)){                                
+                setState({
+                    ...loginData,
+                    errorMessages:{
+                        ...loginData.errorMessages,
+                        invalidPassword:'Type a valid password*'
+                    }
+                })  
+            }else{
+                setState({
+                    ...loginData,
+                    password:value,
+                    errorMessages:{
+                        ...loginData.errorMessages,
+                        invalidPassword:''
+                    }
+                })  
+            }
+        }
+    }
+
+    const { email, password } = loginData;
+    const { invalidEmail, invalidPassword } = loginData.errorMessages;
+
+    return(        
         <div className="LoginContainer">
             <div className='formContainer'>
                 <form className='rounded p-5'>
                     <div className='d-flex justify-content-start align-items-end mb-3'>                           
                         <p className='m-1 text-white h5 font-weight-light'><span className='font-weight-light display-4'>Agenda</span>  Login</p>           
                     </div>                                             
-                    <div class="input-group mb-2">                    
+                    <div class="input-group">                    
                         <div class="input-group-prepend">
                             <div class="input-group-text">
                                 <ion-icon name="mail-outline"></ion-icon>                            
                             </div>
-                        </div>
+                        </div>                        
                         <input type="email" name="email" class="form-control" placeholder="Enter email" onChange={catchInputs}/>                    
-                    </div>
+                    </div>  
+                    {loginData.errorMessages.invalidEmail.length ? (
+                        <small className='text-danger'>{loginData.errorMessages.invalidEmail}</small>                 
+                    ):null}                    
 
-                    <div class="input-group mb-2">
+                    <div class="input-group mt-2">
                         <div class="input-group-prepend">
                             <div class="input-group-text">
                                 <ion-icon name="bag-outline"></ion-icon>
@@ -53,9 +113,12 @@ export default function Login(){
                         </div>
                         <input type="password" name="password" class="form-control" placeholder="Password" onChange={catchInputs}/>                    
                     </div>
+                    {loginData.errorMessages.invalidPassword.length ? (
+                        <small className='text-danger'>{loginData.errorMessages.invalidPassword}</small>                 
+                    ):null}                                        
                     
                     <div className='d-flex justify-content-end mt-3'>                    
-                        <button type="submit" className="btn btn-success w-100" onClick={Login}>Login</button>
+                        <button type="submit" className="btn btn-success w-100" disabled={ !email.length || !password.length || invalidPassword.length || invalidEmail.length ? true : false} onClick={Login}>Login</button>
                     </div>    
 
                     <div className='registerTextContainer mt-1'>
